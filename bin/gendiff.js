70089f3c554d85d51643b4b6f4013f92d65a0a4f
FasTrss <greyhollmen@gmail.com>
2022-11-28 10:32:12 +0300

    add file1 and file2 JSON

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -3,7 +3,10 @@ import { Help, program } from 'commander';
 
 program
   .version('0.0.1')
-  .description('Compares two configuration files and shows a difference.');
+  .description('Compares two configuration files and shows a difference.')
+  .option('-f, --format <type>', 'output format')
+  .arguments('<filepath1>')
+  .arguments('<filepath2>')
 
   program.parse();
   

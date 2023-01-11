026a6826800ee2201b4c0a784129e5fe6299c7a4
FasTrss <greyhollmen@gmail.com>
2023-01-11 19:22:10 +0300

    fix test-cov

--- src/getDiff.js
+++ src/getDiff.js
@@ -7,39 +7,20 @@ const getDiff = (file1, file2) => {
     const value1 = file1[key];
     const value2 = file2[key];
     if (_.isObject(value1) && _.isObject(value2)) {
-      return {
-        type: 'nested',
-        key,
-        children: getDiff(value1, value2),
-      };
+      return { type: 'nested', key, children: getDiff(value1, value2) };
     }
     if (!_.has(file2, key)) {
-      return {
-        type: 'removed',
-        key,
-        value1,
-      };
+      return { type: 'removed', key, value1 };
     }
     if (!_.has(file1, key)) {
-      return {
-        type: 'added',
-        key,
-        value2,
-      };
+      return { type: 'added', key, value2 };
     }
     if (value1 !== value2) {
       return {
-        type: 'changed',
-        key,
-        value1,
-        value2,
+        type: 'changed', key, value1, value2,
       };
     }
-    return {
-      type: 'unchanged',
-      key,
-      value1,
-    };
+    return { type: 'unchanged', key, value1 };
   });
   return result;
 };
